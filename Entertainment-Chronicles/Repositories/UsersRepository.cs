d16471c4918b503f195037b62a8c90dd78cdc59e
dmaceylewis <danna.macey.lewis@gmail.com>
2024-10-04 10:31:51 -0400

    added DbUtils and started repos

---
+++ Entertainment-Chronicles/Repositories/UsersRepository.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.SqlClient;
+using Entertainment_Chronicles.Models;
+using Entertainment_Chronicles.Utils;
+
+namespace Entertainment_Chronicles.Repositories
+{
+    public class UsersRepository
+    {
+
+    }
+}
--- Entertainment-Chronicles/obj/Debug/net8.0/Entertainment-Chronicles.AssemblyInfo.cs
+++ Entertainment-Chronicles/obj/Debug/net8.0/Entertainment-Chronicles.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entertainment-Chronicles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cbccceefacb1d03a0f1de2d770a28adc52a825e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a5f6c830ed95bc1e53b871845881f26bc0de52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entertainment-Chronicles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entertainment-Chronicles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
